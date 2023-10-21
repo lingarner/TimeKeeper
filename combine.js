1f5941b968c78d2cb9c7e6376ffe423defff0b30
Lindsay Garner <gar21034@byui.edu>
2023-10-21 13:43:49 -0600

    cool

--- combine.js
+++ combine.js
@@ -327,6 +327,11 @@ const addProjectButton = document.getElementById("addProjectButton");
 addProjectButton.addEventListener("click", () => {
     addDataToProject();
     document.getElementById('projectName').value = "";
+    document.getElementById('goalTime').value = "";
+    // Reset the select element to its default option
+    document.querySelector('.choose_color').selectedIndex = 0;
+    document.querySelector(".formContainer").style.display = "none";
+
 });
 
 
@@ -361,9 +366,86 @@ function deleteData() {
 
 }
 
-const addProject = document.querySelector(".material-symbols-outlined");
+const add = document.getElementById('add');
 const formContainer = document.querySelector(".formContainer");
-addProject.addEventListener("click", function (){
-    formContainer.style.display = "block";
-    textInput.focus();
+add.addEventListener("click", function (){
+    if (formContainer.style.display === "block") {
+        formContainer.style.display = "none"; // Hide the form
+    } else {
+        formContainer.style.display = "block"; // Show the form
+        // If you want to focus on an input field within the form
+        const textInput = formContainer.querySelector("input");
+        if (textInput) {
+        textInput.focus();
+        }
+        
+    }
 });
+
+
+// Update the project title on the form page
+// set fields from elements in html
+var inputField = document.getElementById("projectName");
+var outputText = document.getElementById("projectTitle")
+
+// check for changes
+inputField.addEventListener("input", function() {
+    // Get the value from the input field
+    var inputValue = inputField.value;
+
+    // Set the value of the h1 element to the input value
+    outputText.textContent = inputValue;
+});
+
+// Update the color of the progress bar
+
+let mySelect = document.querySelector('.choose_color'); // You can change the value of 'condition' to switch between cases
+
+// create bar parts in js
+let goalBox = document.getElementById("goalBox");
+let progressBox = document.getElementById("progressBox");
+
+// add listener to check if the select color has been adjusted        
+mySelect.addEventListener("change",function(){
+    // set swiitch condition
+    let condition = mySelect.value;
+    
+    switch (condition) {
+        case "blue":
+        progressBox.style.backgroundColor = "#1d6a9e";
+        goalBox.style.backgroundColor = "#57a0d1";
+        console.log("blue");
+        break;
+        case "red":
+            progressBox.style.backgroundColor = "#F24F31";
+            goalBox.style.backgroundColor = "#FCC3B9";
+            console.log("REd");
+            break;
+        case "orange":
+            progressBox.style.backgroundColor = "#F08837";
+            goalBox.style.backgroundColor = "#EABF9D";
+            break;
+        case "yellow":
+            progressBox.style.backgroundColor = "#F7F571";
+            goalBox.style.backgroundColor = "#EAEABA";
+            break;
+                                
+        case "green":
+            progressBox.style.backgroundColor = "#479C30";
+            goalBox.style.backgroundColor = "#A1CF94";
+            break;
+        case "purple":
+            progressBox.style.backgroundColor = "#4428A5";
+            goalBox.style.backgroundColor = "#988CC2";
+            break;
+        case "black":
+            progressBox.style.backgroundColor = "#000000";
+            goalBox.style.backgroundColor = "#818181"; // Default color if condition doesn't match any case  
+            break;                  
+        default:
+            progressBox.style.backgroundColor = "#000000";
+            goalBox.style.backgroundColor = "#818181"; // Default color if condition doesn't match any case
+    }
+});
+
+
