e8e360f279e661c4528f6e4c3654e2df0af7cb4c
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-10-20 20:54:31 -0600

    attempt to use storage
    Co-authored-by: elyssahancock <elyssahancock@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record=await res.json();
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();
+
+function saveToJson() {
+    // Get the input data
+    const projectName = document.getElementById("projectName").value;
+
+    // Create a JSON object
+    const jsonData = {
+        data: projectName
+    };
+
+    // Convert the JSON object to a JSON string
+    const jsonString = JSON.stringify(jsonData, null, 2);
+
+    // Display the JSON string
+    document.getElementById("output").textContent = jsonString;
+    console.log("Project name", jsonString); //test
+    console.log("Text Value:", projectName);
+
+};
+
+// Get the button element by its ID
+const addProjectButton = document.getElementById("addProjectButton");
+
+// Add an event listener for the click event
+addProjectButton.addEventListener("click", addDataToStorage);
+
+// To add data to storage
+function addDataToStorage() {
+    const projectName = document.getElementById("projectName").value;
+  
+    // Create a JSON object
+    const jsonData = {
+      data: projectName
+    };
+  
+    // Display the JSON string
+    document.getElementById("output").textContent = JSON.stringify(jsonData, null, 2);
+  
+    // Store the JSON object directly in sync storage
+    chrome.storage.sync.set({ "name": jsonData }, function() {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      } else {
+        console.log("Data saved successfully.");
+      }
+    });
+  }
